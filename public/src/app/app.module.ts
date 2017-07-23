279a188f695ea0ff4442779128aee1250809a228
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-23 16:29:55 -0500

    ang working

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,8 +2,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { FormsModule } from "angular/forms";
-
+import { FormsModule } from "@angular/forms";
 // Components
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
