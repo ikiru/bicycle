99c4fbe189f60d05789b6db678a66f4f815c4686
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-24 14:57:00 -0500

    login and reg working and passing data

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { UserService } from "./user.service";
 
 @Component({
   selector: "app-root",
@@ -7,9 +8,27 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "app";
+  current_user = {};
 
+  constructor(private _userService: UserService) {}
+
+  propagateUser(user) {
+    this.current_user = user;
+  }
   createUser(newUser) {
     console.log("in app component");
     console.log(newUser);
+    return this._userService
+      .create(newUser)
+      .then(user => {
+        console.log(user);
+        if (user.errors) {
+        } else {
+          this.propagateUser(user);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { UserService } from "../user.service";
 
 @Component({
   selector: "app-login",
@@ -9,11 +10,55 @@ export class LoginComponent implements OnInit {
   @Output() createUserEvent = new EventEmitter();
   loginUser = {};
   newUser = {};
+  registrationErrors = [];
+  loginErrors = [];
 
-  constructor() {}
+  constructor(private _userService: UserService) {}
 
-  createUser(newUser) {
+  propagateUser(newUser) {
     this.createUserEvent.emit(newUser);
+  }
+
+  // login the user
+  login(loginUser) {
+    console.log(loginUser);
+    return this._userService
+      .authenticate(loginUser)
+      .then(data => {
+        if (data.errors) {
+          for (let key in data.errors) {
+            let error = data.errors[key];
+            this.loginErrors.push(error.message);
+          }
+        } else {
+          this.propagateUser(data);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  // create the user
+  createUser(newUser) {
+    console.log("in app component");
+    console.log(newUser);
+    return this._userService
+      .create(newUser)
+      .then(user => {
+        console.log(user);
+        if (user.errors) {
+          for (let key in user.errors) {
+            let error = user.errors[key];
+            this.registrationErrors.push(error.message);
+          }
+        } else {
+          this.propagateUser(user);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   ngOnInit() {}
